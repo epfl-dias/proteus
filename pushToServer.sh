56b5af4a0c0d4aea32af7961b93bc073ed4f0727
manosk <manos.karpathiotakis@epfl.ch>
2015-09-07 23:17:20 +0200

    push to server script

---
+++ pushToServer.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+scp -r common  karpathi@diascld31.epfl.ch:/cloud_store/manosk/vida-engine/sources/raw-jit-executor/
+scp -r expressions  karpathi@diascld31.epfl.ch:/cloud_store/manosk/vida-engine/sources/raw-jit-executor/
+scp -r values  karpathi@diascld31.epfl.ch:/cloud_store/manosk/vida-engine/sources/raw-jit-executor/
+scp -r operators  karpathi@diascld31.epfl.ch:/cloud_store/manosk/vida-engine/sources/raw-jit-executor/
+scp -r jsmn  karpathi@diascld31.epfl.ch:/cloud_store/manosk/vida-engine/sources/raw-jit-executor/
+scp -r main*.cpp   karpathi@diascld31.epfl.ch:/cloud_store/manosk/vida-engine/sources/raw-jit-executor/
+scp -r tpch*.cpp   karpathi@diascld31.epfl.ch:/cloud_store/manosk/vida-engine/sources/raw-jit-executor/
+scp -r common  karpathi@diascld31.epfl.ch:/cloud_store/manosk/vida-engine/sources/raw-jit-executor/
+scp -r util  karpathi@diascld31.epfl.ch:/cloud_store/manosk/vida-engine/sources/raw-jit-executor/
+scp -r memory  karpathi@diascld31.epfl.ch:/cloud_store/manosk/vida-engine/sources/raw-jit-executor/
+scp -r plugins  karpathi@diascld31.epfl.ch:/cloud_store/manosk/vida-engine/sources/raw-jit-executor/
+scp -r tests  karpathi@diascld31.epfl.ch:/cloud_store/manosk/vida-engine/sources/raw-jit-executor/
+scp -r CMakeLists.txt  karpathi@diascld31.epfl.ch:/cloud_store/manosk/vida-engine/sources/raw-jit-executor/
