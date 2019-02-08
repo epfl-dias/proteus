2ad38fafb10e316569ddd30ae77eaff5984c2b2c
Lionel Sambuc <lionel.sambuc@epfl.ch>
2019-02-08 14:25:22 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Aeolus Project
+
+## Requirements
+
+ * Ubuntu 18.04 LTS
+ * Git
+ * GNU Make
+ * LLVM
+ * Google Tests
+ * Google Log
+
+## Building the project
+
+## Folder layout
+
+### After checkout of the project
+
+```
+    .
+    ├── CMakeLists.txt
+    ├── LICENSE
+    └── README.md
+```
+
+### After a successfull build of the project
+
+Using the default settings, here is a summary of the folder hierarchy which
+will be created:
+```shell
+    .
+    ├── CMakeLists.txt
+    ├── LICENSE
+    └── README.md
+```
---
+++ tools/check_licenses.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+
+import getopt
+import os
+import re
+import sys
+
+
+def usage(progname, retval=0):
+    print("%s [-r <folder>]" % progname)
+    print("\t-r <folder>           \tBase folder for which to check the licenses,"
+          "\r\t                    \tby default the current working "
+          "directory.")
+    print("")
+
+    sys.exit(retval)
+
+
+#############################################################################
+# Manage parameters & global symbols
+
+def check(root):
+    with open("LICENSE") as file_name:
+        header = file_name.read()
+
+    # Replace years with regex
+    copyright_match = r"""Copyright \(c\) \d+-\d+"""
+    copyright_insert = r"""Copyright (c) \d+-\d+"""
+    header = re.sub(copyright_match, copyright_insert, header)
+
+    # Reconstruct C++ header
+    header = r"""^/\*
+.*\n?""" + header + r""" \*/\n"""
+    header = re.sub(r"([()])", r"\\\g<1>", header)
+
+    header = re.compile(header, re.MULTILINE)
+
+    exts = [".cpp", ".hpp", ".cu", ".cuh", ".c", ".h"]
+
+    # Files that should not contain the header (usually files from external
+    # projects)
+    external_files = [
+    ]
+
+    found_bad = False
+
+    for d, subdirs, files in os.walk(root, followlinks=False):
+        for file_name in files:
+            for ext in exts:
+                if file_name.endswith(ext):
+                    p = os.path.join(d, file_name)
+                    relpath = os.path.relpath(p, root)
+                    with open(p) as file_handle:
+                        if relpath in external_files:
+                            # files in external_files are from external projects
+                            # do not use our header
+                            if header.search(file_handle.read()):
+                                print("Remove our license: " + str(relpath))
+                                found_bad = True
+                        else:
+                            if not header.search(file_handle.read()):
+                                print("Missing license: " + str(relpath))
+                                found_bad = True
+
+    if found_bad:
+        sys.exit(-1)
+
+
+def main(argv):
+    progname = argv[0]
+    root = os.getcwd()
+
+    try:
+        opts, args = getopt.getopt(argv[1:], 'r:')
+    except getopt.GetoptError:
+        usage(progname, 1)
+
+    for opt, arg in opts:
+        if opt == '-r':
+            root = arg
+        elif opt == '-h':
+            usage(progname)
+        else:
+            usage(progname, 1)
+
+    root = os.path.realpath(root)
+    assert(os.path.isdir(root))
+
+    check(root)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
