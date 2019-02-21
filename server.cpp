3621c91a437d678ad3d15cf5fa6a2d2563e108df
raza <syed.raza@epfl.ch>
2019-02-21 12:47:58 +0100

    license correction

--- server.cpp
+++ server.cpp
@@ -1,4 +1,22 @@
+/*
+                             Copyright (c) 2019-2019
+           Data Intensive Applications and Systems Laboratory (DIAS)
+                   Ecole Polytechnique Federale de Lausanne
 
+                              All Rights Reserved.
+
+      Permission to use, copy, modify and distribute this software and its
+    documentation is hereby granted, provided that both the copyright notice
+  and this permission notice appear in all copies of the software, derivative
+  works or modified versions, and any portions thereof, and that both notices
+                      appear in supporting documentation.
+
+  This code is distributed in the hope that it will be useful, but WITHOUT ANY
+ WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ A PARTICULAR PURPOSE. THE AUTHORS AND ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE
+DISCLAIM ANY LIABILITY OF ANY KIND FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE
+                             USE OF THIS SOFTWARE.
+*/
 
 #include <unistd.h>
 #include <functional>
