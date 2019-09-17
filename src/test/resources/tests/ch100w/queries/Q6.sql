4bb02153593c3a5c88658e09fe55198af51c5ca4
Periklis Chrysogelos <periklis93@yahoo.gr>
2019-09-18 00:16:52 +0200

    Remove tests until CI has the relevant catalogs

--- src/test/resources/tests/ch100w/queries/Q6.sql
+++
@@ -1,5 +0,0 @@
-select	sum(ol_amount) as revenue
-from	ch100w_orderline
-where	ol_delivery_d >= timestamp '1999-01-01 00:00:00.000000'
-	and ol_delivery_d < timestamp '2020-01-01 00:00:00.000000'
-	and ol_quantity between 1 and 100000 ;
--- src/test/resources/tests/genom/queries/Q1.sql
+++
@@ -1,6 +0,0 @@
-SELECT count(*)
-FROM genom_broad as L, genom_broad2 as R
-WHERE L.chrom = R.chrom2
-  AND L.`start` < R.stop2
-  AND R.`start2` < L.stop
-  AND L.filename = 'ENCFF271CVJ.bed' ;
