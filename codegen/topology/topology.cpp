c467154bb01aa340b62e9a2c336c41fc268f1d5d
Periklis Chrysogelos <periklis93@yahoo.gr>
2019-10-16 21:11:16 +0200

    Fix cpucore_index

--- codegen/topology/topology.cpp
+++ codegen/topology/topology.cpp
@@ -181,7 +181,7 @@ void topology::init_() {
   cpucore_index.resize(core_cnt);
   for (const auto &cpu : cpu_info) {
     for (const auto &core : cpu.local_cores) {
-      cpucore_index[cpu.id] = core_info.size();
+      cpucore_index[core] = core_info.size();
       core_info.emplace_back(core, cpu.id, core_info.size());
     }
   }
