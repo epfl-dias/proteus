fa93d99e07eadb927948d6b1caadb0bab883393c
Aunn Raza <aunnraza@gmail.com>
2020-02-27 15:04:50 +0100

    merged OLTP-memory-manager into header-only file

--- core/oltp/engine/include/storage/memory_manager.hpp
+++ core/oltp/engine/include/storage/memory_manager.hpp
@@ -25,16 +25,47 @@
 #define STORAGE_MEMORY_MANAGER_HPP_
 
 #include <sys/mman.h>
-
 #include <new>
+
+#include "memory/memory-manager.hpp"
+#include "topology/affinity_manager.hpp"
+#include "topology/topology.hpp"
 
 namespace storage::memory {
 
 class MemoryManager {
  public:
   static void *alloc(size_t bytes, int numa_memset_id,
-                     int mem_advice = MADV_DOFORK | MADV_HUGEPAGE);
-  static void free(void *mem);
+                     int mem_advice = MADV_DOFORK | MADV_HUGEPAGE){
+    void* ret = nullptr;
+
+    // const auto& vec = scheduler::Topology::getInstance().getCpuNumaNodes();
+    // assert(numa_memset_id < vec.size());
+    // void* ret = numa_alloc_onnode(bytes, vec[numa_memset_id].id);
+
+    // if (madvise(ret, bytes, mem_advice) == -1) {
+    //   fprintf(stderr, "%s:%d %s\n", __FILE__, __LINE__, strerror(errno));
+    //   assert(false);
+    //   return nullptr;
+    // }
+
+    // return numa_alloc_interleaved(bytes);
+
+
+    if (numa_memset_id >= 0) {
+      static const auto& nodes = topology::getInstance().getCpuNumaNodes();
+      set_exec_location_on_scope d{nodes[numa_memset_id]};
+      ret = ::MemoryManager::mallocPinned(bytes);
+    } else {
+      ret = ::MemoryManager::mallocPinned(bytes);
+    }
+    return ret;
+  }
+
+  static void free(void *mem){
+    // numa_free(mem, bytes);
+    ::MemoryManager::freePinned(mem);
+  }
 };
 
 struct mem_chunk {
