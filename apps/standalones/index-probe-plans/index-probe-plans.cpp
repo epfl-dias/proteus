7c223ec0cad7d048e42fd4c49d4f2a1c59e1d5c8
Periklis Chrysogelos <periklis93@yahoo.gr>
2020-09-15 19:13:33 +0200

    Add main for index-probe testbed

---
+++ apps/standalones/index-probe-plans/index-probe-plans.cpp
@@ -0,0 +1,42 @@
+/*
+    Proteus -- High-performance query processing on heterogeneous hardware.
+
+                            Copyright (c) 2020
+        Data Intensive Applications and Systems Laboratory (DIAS)
+                École Polytechnique Fédérale de Lausanne
+
+                            All Rights Reserved.
+
+    Permission to use, copy, modify and distribute this software and
+    its documentation is hereby granted, provided that both the
+    copyright notice and this permission notice appear in all copies of
+    the software, derivative works or modified versions, and any
+    portions thereof, and that both notices appear in supporting
+    documentation.
+
+    This code is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. THE AUTHORS
+    DISCLAIM ANY LIABILITY OF ANY KIND FOR ANY DAMAGES WHATSOEVER
+    RESULTING FROM THE USE OF THIS SOFTWARE.
+*/
+
+#include <cli-flags.hpp>
+#include <topology/affinity_manager.hpp>
+#include <topology/topology.hpp>
+
+int main(int argc, char *argv[]) {
+  auto ctx = proteus::from_cli::olap("Template", &argc, &argv);
+
+  set_exec_location_on_scope exec(topology::getInstance().getCpuNumaNodes()[0]);
+
+  // CREATE TABLE T(a INTEGER, b INTEGER)
+
+  // INSERT INTO T VALUES (5, 10);
+
+  // SELECT * FROM T WHERE a=5;
+
+  // UPDATE T SET b = 15 WHERE a=5;
+
+  return 0;
+}
