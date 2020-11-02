19ca04a29dfcfba92bb4ec4ae370b695649043a7
Periklis Chrysogelos <periklis93@yahoo.gr>
2020-11-02 13:35:08 +0100

    Reduce duplicate code across cmake lists

--- cmake/modules/proteus-lib.cmake
+++ cmake/modules/proteus-lib.cmake
@@ -3,6 +3,10 @@ set_target_properties(${PROJECT_NAME}
         SOVERSION ${${PROJECT_NAME}_VERSION_MAJOR}
         VERSION   ${${PROJECT_NAME}_VERSION})
 
+# Error on undefined symbols in shared libraries
+set_target_properties(${PROJECT_NAME} PROPERTIES
+    LINK_FLAGS "-Wl,--no-allow-shlib-undefined -Wl,-z,defs")
+
 # Define headers for this library. PUBLIC headers are used for
 # compiling the library, and will be added to consumers' build
 # paths.
