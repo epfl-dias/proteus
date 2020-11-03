8ce70077def1c77e5943ccfb9c59901e70df77fc
Periklis Chrysogelos <periklis93@yahoo.gr>
2020-11-03 21:46:34 +0100

    Add namespace in c++ clion template

--- .idea/fileTemplates/internal/C++ Class Header.h
+++ .idea/fileTemplates/internal/C++ Class Header.h
@@ -2,12 +2,20 @@
 #[[#ifndef]]# ${INCLUDE_GUARD}
 #[[#define]]# ${INCLUDE_GUARD}
 
+#if (${NAMESPACES_OPEN} == "")
+namespace ${PROJECT_NAME} {
+#else
 ${NAMESPACES_OPEN}
+#end
 
 class ${NAME} {
 
 };
 
+#if (${NAMESPACES_OPEN} == "")
+}  // namespace ${PROJECT_NAME}
+#else
 ${NAMESPACES_CLOSE}
+#end
 
 #[[#endif]]# /* ${INCLUDE_GUARD} */
