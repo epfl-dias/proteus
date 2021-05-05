8217013bb180ae95e40f4d980d562c71ca9a2e7e
Periklis Chrysogelos <periklis93@yahoo.gr>
2021-05-05 20:35:26 +0200

    Exclude Fetched Content from ALL target

--- cmake/modules/proteus-prolog.cmake
+++ cmake/modules/proteus-prolog.cmake
@@ -8,14 +8,27 @@
 # Set LLVM suffix for systems with multiple LLVM installed to request
 # a specific version using -DLLVM_VERSION_SUFFIX=7
 if (DEFINED LLVM_VERSION_SUFFIX)
-	set(LLVM_REQUIRED_VERSION "${LLVM_VERSION_SUFFIX}")
-	set(LLVM_VERSION_SUFFIX "-${LLVM_VERSION_SUFFIX}")
-else()
-	set(LLVM_REQUIRED_VERSION 11)
-endif()
+  set(LLVM_REQUIRED_VERSION "${LLVM_VERSION_SUFFIX}")
+  set(LLVM_VERSION_SUFFIX "-${LLVM_VERSION_SUFFIX}")
+else ()
+  set(LLVM_REQUIRED_VERSION 11)
+endif ()
 
 # FIXME: Should we use find_package and ${LLVM_TOOLS_BINARY_DIR} to find clang?
 find_program(CLANG_CXX_COMPILER "clang++${LLVM_VERSION_SUFFIX}" REQUIRED)
 set(CMAKE_CXX_COMPILER "${CLANG_CXX_COMPILER}")
 find_program(CLANG_C_COMPILER "clang${LLVM_VERSION_SUFFIX}" REQUIRED)
 set(CMAKE_C_COMPILER "${CLANG_C_COMPILER}")
+
+function(FetchContent name)
+  # Check if population has already been performed
+  FetchContent_GetProperties(${name})
+  string(TOLOWER "${name}" lcName)
+  if (NOT ${lcName}_POPULATED)
+    # Fetch the content using previously declared details
+    FetchContent_Populate(${name})
+
+    # Bring the populated content into the build
+    add_subdirectory(${${lcName}_SOURCE_DIR} ${${lcName}_BINARY_DIR} EXCLUDE_FROM_ALL)
+  endif ()
+endfunction()
