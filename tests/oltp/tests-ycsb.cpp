56cf0ec5a44d48e6d8bd2dcbae069f82f10b699f
Aunn Raza <aunnraza@gmail.com>
2021-05-09 19:03:33 +0200

    Add YCSB unit test.

--- tests/oltp/tests-ycsb.cpp
+++ tests/oltp/tests-ycsb.cpp
@@ -21,7 +21,48 @@
     RESULTING FROM THE USE OF THIS SOFTWARE.
 */
 
+#include "oltp.hpp"
 #include "test-utils.hpp"
 #include "ycsb.hpp"
 
-TEST(YCSB, pseudo) { EXPECT_TRUE(true); }
+constexpr size_t runtime_sec = 10;
+
+::testing::Environment* const pools_env =
+    ::testing::AddGlobalTestEnvironment(new TestEnvironment);
+
+TEST(YCSB, ycsb_all_socket_50RW_50Zipf) {
+  size_t num_records = 1000000;
+  double theta = 0.5;
+  double rw_ratio = 0.5;
+  size_t num_cols = 10;
+  size_t ops_per_txn = 10;
+
+  EXPECT_NO_THROW({
+    auto oltp_num_workers = topology::getInstance().getCoreCount();
+    auto npartitions = topology::getInstance().getCpuNumaNodes().size();
+    LOG(INFO) << "N_Partitions: " << npartitions;
+
+    num_records = num_records * oltp_num_workers;
+
+    g_num_partitions = npartitions;
+    OLTP oltp_engine{};
+    bench::Benchmark* bench =
+        new bench::YCSB("YCSB", num_cols, num_records, theta, 0, ops_per_txn,
+                        rw_ratio, oltp_num_workers, oltp_num_workers,
+                        npartitions, true, num_cols, num_cols, 0);
+
+    oltp_engine.init(bench, oltp_num_workers, npartitions);
+    LOG(INFO) << "[OLTP] Initialization completed.";
+
+    oltp_engine.print_storage_stats();
+    oltp_engine.run();
+
+    usleep((runtime_sec)*1000000);
+    oltp_engine.shutdown(true);
+
+    bench->deinit();
+    delete bench;
+  }
+
+  );
+}
