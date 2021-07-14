b651ed5620d5e2aea47950f062a1dfb4dd779428
Periklis Chrysogelos <periklis93@yahoo.gr>
2021-07-14 19:05:55 +0200

    Export google benchmark target to allow exporting benchmarking libs

--- cmake/modules/googlebenchmark.cmake
+++ cmake/modules/googlebenchmark.cmake
@@ -4,3 +4,5 @@ set(BENCHMARK_ENABLE_TESTING OFF CACHE BOOL "" FORCE)
 set(BENCHMARK_USE_LIBCXX ${USE_LIBCXX} CACHE BOOL "" FORCE)
 
 include(external/CMakeLists.txt.googlebenchmark.in)
+
+export(TARGETS benchmark FILE GoogleBenchmarkConfig.cmake)
