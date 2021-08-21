f25421169cfba6c391dd5227f79d0cf7d7c6235d
Aunn Raza <aunnraza@gmail.com>
2021-08-21 21:19:51 +0200

    Fix aligned allocation in proteus::memory::allocator

--- core/platform/include/platform/memory/allocator.hpp
+++ core/platform/include/platform/memory/allocator.hpp
@@ -24,6 +24,7 @@
 #ifndef PROTEUS_ALLOCATOR_HPP
 #define PROTEUS_ALLOCATOR_HPP
 
+#include <exception>
 #include <limits>
 #include <platform/memory/memory-manager.hpp>
 #include <platform/topology/affinity_manager.hpp>
@@ -32,51 +33,81 @@
 namespace proteus::memory {
 
 template <typename T>
-class ExplicitSocketPinnedMemoryAllocator {
- private:
-  const int numa_memset_id;
-
- public:
-  typedef T value_type;
-  inline explicit ExplicitSocketPinnedMemoryAllocator(int numa)
-      : numa_memset_id(numa) {}
-  template <class U>
-  constexpr ExplicitSocketPinnedMemoryAllocator(
-      const ExplicitSocketPinnedMemoryAllocator<U> &o) noexcept
-      : numa_memset_id(o.numa_memset_id) {}
-  [[nodiscard]] T *allocate(size_t n) {
-    if (n > std::numeric_limits<size_t>::max() / sizeof(T))
-      throw std::bad_alloc();
-    if (numa_memset_id >= 0) {
-      static const auto &nodes = topology::getInstance().getCpuNumaNodes();
-      set_exec_location_on_scope d{nodes[numa_memset_id]};
-      return static_cast<T *>(MemoryManager::mallocPinned(n * sizeof(T)));
-    } else {
-      return static_cast<T *>(MemoryManager::mallocPinned(n * sizeof(T)));
-    }
-  }
-  template <typename U>
-  friend class ExplicitSocketPinnedMemoryAllocator;
-  void deallocate(T *mem, size_t) noexcept { MemoryManager::freePinned(mem); }
-};
-
-template <typename T>
 class PinnedMemoryAllocator {
  public:
   typedef T value_type;
 
   PinnedMemoryAllocator() = default;
   template <class U>
-  constexpr PinnedMemoryAllocator(const PinnedMemoryAllocator<U> &) noexcept {}
+  constexpr explicit PinnedMemoryAllocator(
+      const PinnedMemoryAllocator<U> &o) noexcept
+      : _allocMap(o._allocMap), _allocMapMutex(o._allocMapMutex) {}
 
   [[nodiscard]] T *allocate(size_t n) {
     if (n > std::numeric_limits<size_t>::max() / sizeof(T))
       throw std::bad_alloc();
 
-    return static_cast<T *>(MemoryManager::mallocPinned(n * sizeof(T)));
+    size_t sz = alignof(T) + (n * sizeof(T));
+    auto x = MemoryManager::mallocPinned(sz);
+    auto xCpy = x;
+    auto aligned_ptr = std::align(alignof(T), n * sizeof(T), x, sz);
+    assert(aligned_ptr != nullptr);
+    {
+      std::lock_guard<std::mutex> lk(*_allocMapMutex);
+      _allocMap->operator[](aligned_ptr) = xCpy;
+    }
+    return static_cast<T *>(aligned_ptr);
   }
 
-  void deallocate(T *mem, size_t) noexcept { MemoryManager::freePinned(mem); }
+  void deallocate(T *mem, size_t) noexcept {
+    std::lock_guard<std::mutex> lk(*_allocMapMutex);
+    if (_allocMap->contains(mem)) {
+      MemoryManager::freePinned(_allocMap->operator[](mem));
+      _allocMap->erase(mem);
+    } else {
+      std::terminate();
+    }
+  }
+
+ private:
+  std::shared_ptr<std::map<void *, void *>> _allocMap =
+      std::make_shared<std::map<void *, void *>>();
+  std::shared_ptr<std::mutex> _allocMapMutex = std::make_shared<std::mutex>();
+
+  template <typename>
+  friend class PinnedMemoryAllocator;
+};
+
+template <typename T>
+class ExplicitSocketPinnedMemoryAllocator {
+ private:
+  const int numa_memset_id;
+
+ public:
+  typedef T value_type;
+
+  template <class U>
+  constexpr explicit ExplicitSocketPinnedMemoryAllocator(
+      const ExplicitSocketPinnedMemoryAllocator<U> &o) noexcept
+      : numa_memset_id(o.numa_memset_id) {}
+
+  inline explicit ExplicitSocketPinnedMemoryAllocator(int numa)
+      : numa_memset_id(numa) {}
+
+  [[nodiscard]] T *allocate(size_t n) {
+    if (numa_memset_id >= 0) {
+      static const auto &nodes = topology::getInstance().getCpuNumaNodes();
+      set_exec_location_on_scope d{nodes[numa_memset_id]};
+      return _allocator.allocate(n);
+    } else {
+      return _allocator.allocate(n);
+    }
+  }
+
+  void deallocate(T *mem, size_t sz) { _allocator.deallocate(mem, sz); }
+
+ private:
+  PinnedMemoryAllocator<T> _allocator;
 };
 
 }  // namespace proteus::memory
