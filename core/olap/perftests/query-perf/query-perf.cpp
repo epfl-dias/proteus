b6bd6c0f4245c0d25626538df501ce23ce6f7118
Periklis Chrysogelos <periklis93@yahoo.gr>
2021-11-01 02:28:35 +0100

    Name benchmarking parameters

--- core/olap/perftests/query-perf/query-perf.cpp
+++ core/olap/perftests/query-perf/query-perf.cpp
@@ -23,32 +23,24 @@
 
 #include <benchmark/benchmark.h>
 
+#include <olap/util/demangle.hpp>
 #include <platform/util/glog.hpp>
 #include <platform/util/timing.hpp>
 #include <ssb/query.hpp>
 
-class SSBFixture : public benchmark::Fixture {
- public:
-  size_t SF = 100;
-  void actualRun(std::vector<PreparedStatement> &rel, benchmark::State &state);
-};
-
-void SSBFixture::actualRun(std::vector<PreparedStatement> &rel,
-                           benchmark::State &state) {
+void actualRun(PreparedStatement st, benchmark::State &state) {
   std::chrono::milliseconds exeTime{0};
   size_t its = 0;
 
   for ([[maybe_unused]] auto _ : state) {
-    for (auto &st : rel) {
-      std::stringstream ss;
-      {
-        time_block t{[&](const auto &ms) {
-          if (its++) exeTime += ms;
-        }};
-        ss << st.execute();  // PreparedStatement::SilentExecution
-      }
-      std::this_thread::sleep_for(std::chrono::seconds{1});
+    std::stringstream ss;
+    {
+      time_block t{[&](const auto &ms) {
+        if (its++) exeTime += ms;
+      }};
+      ss << st.execute();  // PreparedStatement::SilentExecution
     }
+    std::this_thread::sleep_for(std::chrono::seconds{1});
   }
 
   state.counters["Exec (ms)"] = (exeTime / (its - 1)).count();
@@ -84,90 +76,90 @@ std::unique_ptr<T> make_shaper(
                              std::move(stat));
 }
 
-BENCHMARK_DEFINE_F(SSBFixture, PreparedQuery)
-(benchmark::State &state) {
+#define QALIAS(name, prepFunction)                                \
+  struct name {                                                   \
+    PreparedStatement operator()(proteus::QueryShaper &s) const { \
+      return prepFunction(s);                                     \
+    }                                                             \
+  };
+#define SALIAS(name, shaperType) \
+  struct name {                  \
+    using shaper_t = shaperType; \
+  };
+
+namespace SSB {
+QALIAS(Q1_1, ssb::Query::prepare11);
+QALIAS(Q1_2, ssb::Query::prepare12);
+QALIAS(Q1_3, ssb::Query::prepare13);
+QALIAS(Q2_1, ssb::Query::prepare21);
+QALIAS(Q2_2, ssb::Query::prepare22);
+QALIAS(Q2_3, ssb::Query::prepare23);
+QALIAS(Q3_1, ssb::Query::prepare31);
+QALIAS(Q3_2, ssb::Query::prepare32);
+QALIAS(Q3_3, ssb::Query::prepare33);
+QALIAS(Q3_4, ssb::Query::prepare34);
+QALIAS(Q4_1, ssb::Query::prepare41);
+QALIAS(Q4_2, ssb::Query::prepare42);
+QALIAS(Q4_3, ssb::Query::prepare43);
+}  // namespace SSB
+
+SALIAS(CPUonly, proteus::CPUOnlySingleSever);
+SALIAS(GPUonly, proteus::GPUOnlySingleSever);
+
+template <typename Qprep, typename Shaper>
+void query_perf(benchmark::State &state) {
+  size_t SF = 100;
   auto stats = ssb::Query::getStats(SF);
 
-  std::vector<std::unique_ptr<proteus::QueryShaper>> shapers;
+  auto shaper = make_shaper<typename Shaper::shaper_t>(SF, stats);
 
-  switch (state.range(1)) {
-    case 0:
-      shapers.emplace_back(make_shaper<proteus::CPUOnlySingleSever>(SF, stats));
-      break;
-    case 1:
-      shapers.emplace_back(make_shaper<proteus::GPUOnlySingleSever>(SF, stats));
-      break;
-    default:
-      assert(false);
-  }
-
-  using namespace std::chrono_literals;
-
-  std::vector<std::function<PreparedStatement(proteus::QueryShaper &)>> prep;
-  prep.emplace_back([&] {
-    switch (state.range(0)) {
-      case 0: {
-        return ssb::Query::prepare11;
-      }
-      case 1: {
-        return ssb::Query::prepare12;
-      }
-      case 2: {
-        return ssb::Query::prepare13;
-      }
-      case 3: {
-        return ssb::Query::prepare21;
-      }
-      case 4: {
-        return ssb::Query::prepare22;
-      }
-      case 5: {
-        return ssb::Query::prepare23;
-      }
-      case 6: {
-        return ssb::Query::prepare31;
-      }
-      case 7: {
-        return ssb::Query::prepare32;
-      }
-      case 8: {
-        return ssb::Query::prepare33;
-      }
-      case 9: {
-        return ssb::Query::prepare34;
-      }
-      case 10: {
-        return ssb::Query::prepare41;
-      }
-      case 11: {
-        return ssb::Query::prepare42;
-      }
-      case 12: {
-        return ssb::Query::prepare43;
-      }
-    }
-    assert(false);
-  }());
-
-  std::vector<PreparedStatement> rel;
-
-  for (auto &shaper_ptr : shapers) {
-    auto &shaper = *shaper_ptr;
-
-    {
-      rel.reserve(rel.size() + prep.size());
-      for (auto &p : prep) rel.emplace_back(p(shaper));
-    }
-  }
-
-  actualRun(rel, state);
+  actualRun(Qprep{}(*shaper), state);
 }
 
-#define BENCHMARK_SCALEOUT_MACRO(testcase)   \
-  BENCHMARK_REGISTER_F(SSBFixture, testcase) \
-      ->Unit(benchmark::kMillisecond)        \
-      ->Iterations(5 + 1)                    \
-      ->UseRealTime()
+#define BENCHMARK_NAMED_MACRO(fun, T1name, T1list, T2name, T2list)           \
+  static void *BENCHMARK_PRIVATE_NAME(regBenchmarks) [[maybe_unused]] = [] { \
+    auto f = [](auto T1, auto T2) {                                          \
+      benchmark::internal::RegisterBenchmarkInternal(                        \
+          new benchmark::internal::FunctionBenchmark(                        \
+              ("perftest-" +                                                 \
+               std::filesystem::path{__FILE__}                               \
+                   .filename()                                               \
+                   .replace_extension("")                                    \
+                   .string() +                                               \
+               "/function:" + #fun +                                         \
+                                                                             \
+               "/" #T1name ":" + demangle(typeid(T1).name()) +               \
+                                                                             \
+               "/" #T2name ":" + demangle(typeid(T2).name()))                \
+                  .c_str(),                                                  \
+              &fun<decltype(T1), decltype(T2)>))                             \
+          ->Unit(benchmark::kMillisecond)                                    \
+          ->Iterations(5 + 1)                                                \
+          ->UseRealTime();                                                   \
+    };                                                                       \
+                                                                             \
+    std::apply(                                                              \
+        [&](auto... y) {                                                     \
+          auto fy = [&](auto y2) {                                           \
+            std::apply([&](auto... x) { (..., f(x, y2)); }, T1list{});       \
+          };                                                                 \
+                                                                             \
+          (..., fy(y));                                                      \
+        },                                                                   \
+        T2list{});                                                           \
+    return nullptr;                                                          \
+  }();
 
-BENCHMARK_SCALEOUT_MACRO(PreparedQuery)
-    ->ArgsProduct({{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}, {0, 1}});
+using QuerySet = std::tuple<
+    // Group 1
+    SSB::Q1_1, SSB::Q1_2, SSB::Q1_3,
+    // Group 2
+    SSB::Q2_1, SSB::Q2_2, SSB::Q2_3,
+    // Group 3
+    SSB::Q3_1, SSB::Q3_2, SSB::Q3_3, SSB::Q3_4,
+    // Group 4
+    SSB::Q4_1, SSB::Q4_2, SSB::Q4_3>;
+
+using ShaperSet = std::tuple<CPUonly, GPUonly>;
+
+BENCHMARK_NAMED_MACRO(query_perf, query, QuerySet, shaper, ShaperSet);
